11289045ab9f77d97bbbbd487885fd494ed91257
hadeer-gamal-dot <deerh289@gmail.com>
2026-05-26 05:26:19 +0300

    build craftsman and admin service

---
+++ Harfi.API/Controllers/CraftsmenController.cs
@@ -0,0 +1,54 @@
+using Harfi.DTOs.Craftsman;
+using Harfi.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Harfi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CraftsmenController : ControllerBase
+    {
+        private readonly ICraftsmanService _craftsmanService;
+
+        public CraftsmenController(ICraftsmanService craftsmanService)
+        {
+            _craftsmanService = craftsmanService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] CreateCraftsmanDto dto)
+        {
+            var result = await _craftsmanService.RegisterCraftsmanAsync(dto);
+            if (!result) return BadRequest("He failed to submit the registration application.");
+            return Ok("Your application has been successfully submitted and is currently under review.");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProfile(int id)
+        {
+            var profile = await _craftsmanService.GetCraftsmanProfileAsync(id);
+            if (profile == null) return NotFound("The Craftsman is not Present");
+            return Ok(profile);
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search([FromQuery] CraftsmanFilterDto filter)
+        {
+            var results = await _craftsmanService.GetFilteredCraftsmenAsync(filter);
+            return Ok(results);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProfile(int id, [FromBody] UpdateCraftsmanDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _craftsmanService.UpdateCraftsmanAsync(id, dto);
+            if (!result) return NotFound(new { message = "Craftsman not found" });
+
+            return Ok(new { message = "Profile updated successfully" });
+        }
+
+    }
+}
---
+++ Harfi.DTOs/User/UserProfileDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harfi.DTOs.User
+{
+    public class UserProfileDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; } // لمعرفة صلاحيته (customer / craftsman / admin)
+        public string Phone { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
