28779ebcf816c5c8613e88252ed336f8f4c272c7
Ahmed Dabish <111189633+AhmedDabish@users.noreply.github.com>
2026-05-28 16:59:39 +0300

    adding [ searching for craftsman -steps of solutions ]

---
+++ Harfi.Repositories/Interfaces/ICraftsmanRepository.cs
@@ -0,0 +1,11 @@
+﻿using Harfi.Models.Entities;
+
+namespace Harfi.Repositories.Interfaces;
+
+public interface ICraftsmanRepository
+{
+    Task<List<Craftsman>> GetAllAsync();
+    Task<Craftsman?> GetByIdAsync(int id);
+    Task<List<Craftsman>> GetByIdsAsync(IEnumerable<int> ids);
+    Task<List<Craftsman>> FilterAsync(string? serviceType, string? city, double? minRating);
+}
