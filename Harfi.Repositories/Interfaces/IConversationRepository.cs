ea024fffc085587be9463b7ee485081e48e9561a
EsraaShiref <israashiref@gmail.com>
2026-06-07 02:29:23 +0300

    Add admin conversation access feature

--- Harfi.Repositories/Interfaces/IConversationRepository.cs
+++ Harfi.Repositories/Interfaces/IConversationRepository.cs
@@ -1,9 +1,4 @@
 ﻿using Harfi.Models.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Harfi.Repositories.Interfaces
 {
@@ -11,7 +6,9 @@ namespace Harfi.Repositories.Interfaces
     {
         Task<Conversation?> GetByParticipantsAsync(int jobId, int customerId, int craftsmanId);
         Task<Conversation?> GetByIdWithDetailsAsync(int id);
+        Task<Conversation?> GetByIdWithMessagesAsync(int id);
         Task<IEnumerable<Conversation>> GetUserConversationsAsync(int userId);
         Task<bool> IsParticipantAsync(int conversationId, int userId);
+        IQueryable<Conversation> GetAllConversationsQuery();
     }
 }
