a3670a132d5352c9d6312e9f72975f946eca0b79
EbrahemElbadry11 <beboelbadry85@gmail.com>
2026-06-14 06:20:08 +0300

    adding job details and add some feature

---
+++ Harfi.Repositories/Migrations/20260614014620_updae conv.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Harfi.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class updaeconv : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_IsActive",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Role",
+                table: "Users");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CraftsmanHiddenAt",
+                table: "Conversations",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CustomerHiddenAt",
+                table: "Conversations",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CraftsmanHiddenAt",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerHiddenAt",
+                table: "Conversations");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IsActive",
+                table: "Users",
+                column: "IsActive");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_IsDeleted",
+                table: "Users",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Role",
+                table: "Users",
+                column: "Role");
+        }
+    }
+}
